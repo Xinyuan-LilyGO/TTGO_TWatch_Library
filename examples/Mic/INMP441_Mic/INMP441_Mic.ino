b22f1d04516f75126ceebffdbe8472534e13ef80
lewis <lewishe@outlook.com>
2019-11-08 13:53:24 +0800

    fix inmp441 channel format

--- examples/Mic/INMP441_Mic/INMP441_Mic.ino
+++ examples/Mic/INMP441_Mic/INMP441_Mic.ino
@@ -39,7 +39,7 @@ void setup()
         .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX ),
         .sample_rate =  44100,
         .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
         .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),//I2S_COMM_FORMAT_I2S,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
         .dma_buf_count = 2,
