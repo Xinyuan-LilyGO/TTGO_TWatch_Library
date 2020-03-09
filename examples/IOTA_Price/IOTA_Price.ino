28341126aadcc92a715de17bdb56dd3af09db0c6
lewis he <lewisxhe@outlook.com>
2020-03-09 16:53:12 +0800

    Update IOTA_Price Demo

--- examples/IOTA_Price/IOTA_Price.ino
+++ examples/IOTA_Price/IOTA_Price.ino
@@ -2,13 +2,9 @@
 #pragma mark - Depend ArduinoJson and arduino-coinmarketcap-api libraries
 /*
 cd ~/Arduino/libraries
-git clone https://github.com/witnessmenow/arduino-coinmarketcap-api.git
-git clone -b 5.x https://github.com/bblanchon/ArduinoJson.git
+git clone https://github.com/lewisxhe/CoinMarketCapApi2
+git clone -b 6.x https://github.com/bblanchon/ArduinoJson.git
 */
-
-//! This code is ported from https://github.com/oxinon/IOTA-price-ticker-V2-TTGO-T-Display
-//! This code is for demonstration only, if you like please donate to the original author
-
 
 #include <TTGO.h>
 #include <WiFi.h>
@@ -20,9 +16,12 @@ git clone -b 5.x https://github.com/bblanchon/ArduinoJson.git
 #include "iota.h"
 #include "iota2.h"
 
-WiFiClientSecure client;
+// The new api requires a developer key, so you must apply for a key to use
+// https://pro.coinmarketcap.com/account
+#define APIKEY "YOUR API KEY"
 
-CoinMarketCapApi api(client);
+WiFiClientSecure client;
+CoinMarketCapApi api(client, APIKEY);
 
 // CoinMarketCap's limit is "no more than 10 per minute"
 // Make sure to factor in if you are requesting more than one coin.
@@ -32,31 +31,13 @@ TFT_eSPI *tft = nullptr;
 TTGOClass *ttgo = nullptr;
 //---------------------------------------//
 
-WiFiClient espClient2;
-
 // edit this for your network
 #define wifi_ssid "WIFISSID"
 #define wifi_password "WIFIPASSWD"
 
-
 //---------------------------------------//
-
-// supportet colors
-#define ST7735_BLACK   0x0000
-#define ST7735_GRAY    0x8410
-#define ST7735_WHITE   0xFFFF
-#define ST7735_RED     0xF800
-#define ST7735_ORANGE  0xEC80
-#define ST7735_YELLOW  0xFFE0
-#define ST7735_LIME    0x07FF
-#define ST7735_GREEN   0x07E0
-#define ST7735_CYAN    0x07FF
-#define ST7735_AQUA    0x04FF
-#define ST7735_BLUE    0x001F
-#define ST7735_MAGENTA 0xF81F
-#define ST7735_PINK    0xF8FF
-#define ST7735_CHYN    0x2D05
-
+#define TFT_AQUA  0x04FF
+#define TFT_GRAY  0x8410
 
 // begin setup
 void setup()
@@ -72,14 +53,13 @@ void setup()
 
     uint16_t time = millis();
     time = millis() - time;
-    tft->fillScreen(ST7735_BLACK);
+    tft->fillScreen(TFT_BLACK);
     tft->setSwapBytes(true);
     tft->pushImage(0, 0, iotaWidth, iotaHeight, iota);
     delay(3000);
 
     // Starup
-
-    tft->fillScreen(ST7735_BLACK);
+    tft->fillScreen(TFT_BLACK);
     Serial.print("Connecting to ");
     tft->drawString("Connecting to ", 15, 10, 2);;
     Serial.println(wifi_ssid);
@@ -96,13 +76,13 @@ void setup()
 
     Serial.println("");
     Serial.println("WiFi connected");
-    tft->setTextColor(ST7735_GREEN);
+    tft->setTextColor(TFT_GREEN);
     tft->drawString("WiFi connected", 15, 40, 2);
-    tft->setTextColor(ST7735_WHITE);
+    tft->setTextColor(TFT_WHITE);
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
     delay(1000);
-    tft->fillRect(0, 0, 240, 135, ST7735_BLACK);
+    tft->fillRect(0, 0, 240, 135, TFT_BLACK);
 
     tft->pushImage(0, 0, iota2Width, iota2Height, iota2);
 }
@@ -115,15 +95,10 @@ void printTickerData(String ticker)
     Serial.println("Getting ticker data for " + ticker);
 
 
-    // Ticker unfortunately is not the symbol for some reason.
-    // Go to CoinMarketCap.com and select the coin you would like to check
-    // The ticker name makes up the last part of the URL
-    // e.g: http://coinmarketcap.com/currencies/iota/ , "iota" is the ticker value
+    //For the new API, you can use the currency ID or abbreviated name, such as
+    //Bitcoin, you can view the letter after Circulating Supply at https://coinmarketcap.com/, it is BTC
 
-    // Currency is optional, so you can pass only ticker if you want.
-    // Check out the currency drop down on CoinMarketCap.com to get available values
-
-    CMCTickerResponse response = api.GetTickerInfo(ticker, "usd");
+    CMCTickerResponse response = api.GetTickerInfo(ticker, "USD");
     if (response.error == "") {
         Serial.print("ID: ");
         Serial.println(response.id);
@@ -133,20 +108,18 @@ void printTickerData(String ticker)
         Serial.println(response.symbol);
 
         Serial.print("Rank: ");
-        Serial.println(response.rank);
+        Serial.println(response.cmc_rank);
 
-        Serial.print("Price in USD: ");
-        Serial.println(response.price_usd);
-        Serial.print("Price in BTC: ");
-        Serial.println(response.price_btc);
+        Serial.print("Price: ");
+        Serial.println(response.price);
 
-        Serial.print("24h Volume USD: ");
-        Serial.println(response.volume_usd_24h);
-        Serial.print("Market Cap USD: ");
-        Serial.println(response.market_cap_usd);
+        Serial.print("24h Volume: ");
+        Serial.println(response.volume_24h);
+        Serial.print("Market Cap: ");
+        Serial.println(response.market_cap);
 
-        Serial.print("Available Supply: ");
-        Serial.println(response.available_supply);
+        Serial.print("Circulating Supply: ");
+        Serial.println(response.circulating_supply);
         Serial.print("Total Supply: ");
         Serial.println(response.total_supply);
 
@@ -159,75 +132,73 @@ void printTickerData(String ticker)
         Serial.print("Last Updated: ");
         Serial.println(response.last_updated);
 
-        tft->setTextColor(ST7735_GRAY);
+        tft->setTextColor(TFT_GRAY);
         tft->drawString("MIOTA USD Price", 95, 11, 2);
 
-        tft->setTextColor(ST7735_WHITE);
+        tft->setTextColor(TFT_WHITE);
 
-        tft->fillRect(117, 37, 123, 38, ST7735_BLACK); //price
-        tft->fillRect(185, 80, 55, 20, ST7735_BLACK); //rank
+        tft->fillRect(117, 37, 123, 38, TFT_BLACK); //price
+        tft->fillRect(185, 80, 55, 20, TFT_BLACK); //rank
 
 
 
-        tft->setTextColor(ST7735_YELLOW);
+        tft->setTextColor(TFT_YELLOW);
 
         if (response.percent_change_1h < 0) {
-            tft->setTextColor(ST7735_RED);
+            tft->setTextColor(TFT_RED);
         }
         if (response.percent_change_1h > 0) {
-            tft->setTextColor(ST7735_GREEN);
+            tft->setTextColor(TFT_GREEN);
         }
 
 
-        tft->drawString(String(response.price_usd).c_str(), 117, 37, 6);
-        tft->setTextColor(ST7735_AQUA);
+        tft->drawString(String(response.price).c_str(), 117, 37, 6);
+        tft->setTextColor(TFT_AQUA);
         tft->drawString("Rank:", 117, 80, 4);
 
-        tft->drawString(String(response.rank).c_str(), 190, 80, 4);
+        tft->drawString(String(response.cmc_rank).c_str(), 190, 80, 4);
 
-        tft->drawLine(11, 106, 229, 106, ST7735_GRAY);
+        tft->drawLine(11, 106, 229, 106, TFT_GRAY);
 
-
-
-// hours change
-        tft->fillRect(100, 110, 140, 25, ST7735_BLACK);
-        tft->setTextColor(ST7735_YELLOW);
+        // hours change
+        tft->fillRect(100, 110, 140, 25, TFT_BLACK);
+        tft->setTextColor(TFT_YELLOW);
 
         if (response.percent_change_1h < 0) {
-            tft->setTextColor(ST7735_RED);
+            tft->setTextColor(TFT_RED);
         }
         if (response.percent_change_1h > 0) {
-            tft->setTextColor(ST7735_GREEN);
+            tft->setTextColor(TFT_GREEN);
         }
         tft->drawString("% Price 1h:", 11, 110, 4);
         tft->drawString(String(response.percent_change_1h).c_str(), 156, 110, 4);
         delay(20000);
 
 
-// 24 hours change
-        tft->fillRect(100, 110, 140, 25, ST7735_BLACK);
-        tft->setTextColor(ST7735_YELLOW);
+        // 24 hours change
+        tft->fillRect(100, 110, 140, 25, TFT_BLACK);
+        tft->setTextColor(TFT_YELLOW);
 
         if (response.percent_change_24h < 0) {
-            tft->setTextColor(ST7735_RED);
+            tft->setTextColor(TFT_RED);
         }
         if (response.percent_change_24h > 0) {
-            tft->setTextColor(ST7735_GREEN);
+            tft->setTextColor(TFT_GREEN);
         }
         tft->drawString("% Price 24h:", 11, 110, 4);
         tft->drawString(String(response.percent_change_24h).c_str(), 156, 110, 4);
         delay(20000);
 
 
-// 7d hours change
-        tft->fillRect(100, 110, 140, 25, ST7735_BLACK);
-        tft->setTextColor(ST7735_YELLOW);
+        // 7d hours change
+        tft->fillRect(100, 110, 140, 25, TFT_BLACK);
+        tft->setTextColor(TFT_YELLOW);
 
         if (response.percent_change_7d < 0) {
-            tft->setTextColor(ST7735_RED);
+            tft->setTextColor(TFT_RED);
         }
         if (response.percent_change_7d > 0) {
-            tft->setTextColor(ST7735_GREEN);
+            tft->setTextColor(TFT_GREEN);
         }
         tft->drawString("% Price 7d:", 11, 110, 4);
         tft->drawString(String(response.percent_change_7d).c_str(), 156, 110, 4);
@@ -239,7 +210,7 @@ void printTickerData(String ticker)
     else {
         Serial.print("Error getting data: ");
         Serial.println(response.error);
-        tft->fillRect(200, 2, 40, 32, ST7735_BLACK); //wifi RSSI and alert
+        tft->fillRect(200, 2, 40, 32, TFT_BLACK); //wifi RSSI and alert
         tft->pushImage(203, 2, alertWidth, alertHeight, alert);
     }
     Serial.println("---------------------------------");
@@ -257,7 +228,7 @@ void loop()
         // int signal bars
         Serial.print("WiFi Signal strength: ");
         Serial.print(WiFi.RSSI());
-        tft->fillRect(200, 2, 40, 32, ST7735_BLACK); //wifi RSSI and alert
+        tft->fillRect(200, 2, 40, 32, TFT_BLACK); //wifi RSSI and alert
 
 
         int bars;
@@ -285,10 +256,9 @@ void loop()
 
         // signal bars
         for (int b = 0; b <= bars; b++) {
-            tft->fillRect(202 + (b * 6), 23 - (b * 4), 5, b * 4, ST7735_GRAY);
+            tft->fillRect(202 + (b * 6), 23 - (b * 4), 5, b * 4, TFT_GRAY);
         }
-        printTickerData("iota");
+        printTickerData("MIOTA");
         api_due_time = timeNow + api_mtbs;
     }
-
 }
