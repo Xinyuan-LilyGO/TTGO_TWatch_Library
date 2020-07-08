17f60e0a2cf826c260bfaa86f383a05c314fc202
lewis he <lewisxhe@outlook.com>
2020-07-08 18:19:36 +0800

    Example of adding SIM868 shield

---
+++ examples/Shield/SIM868/config.h
@@ -0,0 +1,19 @@
+
+// => Hardware select
+// #define LILYGO_WATCH_2019_WITH_TOUCH
+//#define LILYGO_WATCH_2019_NO_TOUCH
+
+
+
+//NOT SUPPORT ...
+//#define LILYGO_WATCH_BLOCK
+// #define LILYGO_WATCH_2020_V1
+//NOT SUPPORT ...
+
+
+// => Function select
+#define LILYGO_WATCH_HAS_MOTOR       //To use MOTOR, you need to enable the macro MOTOR
+#define LILYGO_WATCH_HAS_SIM868      //To use SIM868, you need to enable the macro SIM868
+#define LILYGO_WATCH_HAS_SDCARD
+
+#include <LilyGoWatch.h>
