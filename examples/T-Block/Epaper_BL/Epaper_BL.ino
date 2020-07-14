b1c263a198bc1cb891beef4f7625525f9978fcc4
lewis he <lewisxhe@outlook.com>
2020-07-14 14:56:50 +0800

    Update TBlock eink example

--- examples/T-Block/Epaper_BL/Epaper_BL.ino
+++ examples/T-Block/Epaper_BL/Epaper_BL.ino
@@ -7,43 +7,38 @@
 // This code is only suitable for T-Block
 
 cd ~/Arduino/libraries
-git clone https://github.com/ZinggJM/GxEPD2.git
+git clone https://github.com/ZinggJM/GxEPD.git
 git clone https://github.com/olikraus/U8g2_for_Adafruit_GFX.git
-git clone https://github.com/adafruit/Adafruit-GFX-Library.git
 
+1.cd ~/Arduino/libraries/U8g2_for_Adafruit_GFX/src/
+2.Edit GxFont_GFX.h
+3.Uncomment #include <U8g2_for_Adafruit_GFX.h> to use U8G2 font
 
-// Delete the Fonts folder because it conflicts with TWatch
-rm -rf ./Adafruit-GFX-Library/Fonts
-
-* * */
+*/
 
 
 #include "config.h"
 
 extern const unsigned char logoIcon[280];
 
-/******************************************************************
- *                      GXEPD2
- * ***************************************************************/
-// base class GxEPD2_GFX can be used to pass references or pointers to the display instance as parameter, uses ~1.2k more code
-// enable or disable GxEPD2_GFX base class
-#define ENABLE_GxEPD2_GFX 0
+#include <GxEPD.h>
+#include <GxGDEH0154D67/GxGDEH0154D67.h>  // 1.54" b/w
+#include <GxIO/GxIO_SPI/GxIO_SPI.h>
+#include <GxIO/GxIO.h>
 
-#include <GxEPD2_BW.h>
-#include <GxEPD2_3C.h>
-#include <U8g2_for_Adafruit_GFX.h>
 
 #define INK_BUSY                                    34
 #define INK_RESET                                   27
 #define INK_DC                                      19
 #define INK_SS                                      5
 #define INK_MOSI                                    23
-#define INK_MISO                                    19 //eink no use
+#define INK_MISO                                    26 //eink no use
 #define INK_CLK                                     18
 
 #define DEFAULT_BRIGHTNESS                          45
 
-GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(INK_SS,  INK_DC,  INK_RESET, INK_BUSY)); // GDEH0154D67
+GxIO_Class io(SPI,  INK_SS,  INK_DC, INK_RESET);
+GxEPD_Class display(io,  INK_RESET,  INK_BUSY);
 
 U8G2_FOR_ADAFRUIT_GFX u8g2Fonts;
 TTGOClass *twatch = nullptr;
@@ -86,20 +81,14 @@ void mainPage(bool fullScreen)
     static int16_t getX, getY;
     static uint16_t getW, getH;
     char buff[64] = "00:00";
-    bool reverse = false;
     const char *weekChars[] = { "日", "一", "二", "三", "四", "五", "六"};
 
     if (lastSeupCount != seupCount) {
         lastSeupCount = seupCount;
         u8g2Fonts.setFont(u8g2_font_wqy16_t_gb2312a); // select u8g2 font from here: https://github.com/olikraus/u8g2/wiki/fntlistall
-        display.setPartialWindow(getX - getW, getY  - getH  + 1, getW + 1, getH + 1);
-        display.firstPage();
-        do {
-            snprintf(buff, sizeof(buff), "%u 步", seupCount);
-            u8g2Fonts.setCursor(getX - getW, getY);
-            u8g2Fonts.print(buff);
-
-        } while (display.nextPage());
+        snprintf(buff, sizeof(buff), "%u 步", seupCount);
+        u8g2Fonts.setCursor(getX - getW, getY);
+        u8g2Fonts.print(buff);
     }
 
     RTC_Date d = rtc->getDateTime();
@@ -120,77 +109,62 @@ void mainPage(bool fullScreen)
     if (fullScreen) {
         lastX = 25;
         lastY = 100;
-        display.setFullWindow();
-        display.firstPage();
-        do {
-            display.drawBitmap(5, 5, logoIcon, 75, 28, GxEPD_BLACK);
+        display.drawBitmap(5, 5, logoIcon, 75, 28, GxEPD_BLACK);
 
-            //BATTERY ICON
-            u8g2Fonts.setFont(u8g2_font_battery19_tn);
-            u8g2Fonts.setCursor(175, 20);                // start writing at this position
-            u8g2Fonts.setFontDirection(3);              // left to right (this is default)
-            u8g2Fonts.print(4);
-            u8g2Fonts.setFontDirection(0);              // left to right (this is default)
+        //BATTERY ICON
+        u8g2Fonts.setFont(u8g2_font_battery19_tn);
+        u8g2Fonts.setCursor(175, 20);                // start writing at this position
+        u8g2Fonts.setFontDirection(3);              // left to right (this is default)
+        u8g2Fonts.print(4);
+        u8g2Fonts.setFontDirection(0);              // left to right (this is default)
 
 
-            display.drawFastHLine(10, 40, display.width() - 20, GxEPD_BLACK);
-            display.drawFastHLine(10, 150, display.width() - 20, GxEPD_BLACK);
+        display.drawFastHLine(10, 40, display.width() - 20, GxEPD_BLACK);
+        display.drawFastHLine(10, 150, display.width() - 20, GxEPD_BLACK);
 
-            u8g2Fonts.setFont(u8g2_font_inr38_mn  ); // select u8g2 font from here: https://github.com/olikraus/u8g2/wiki/fntlistall
+        u8g2Fonts.setFont(u8g2_font_inr38_mn  ); // select u8g2 font from here: https://github.com/olikraus/u8g2/wiki/fntlistall
 
-            u8g2Fonts.setCursor(lastX, lastY);                // start writing at this position
-            u8g2Fonts.print(buff);
+        u8g2Fonts.setCursor(lastX, lastY);                // start writing at this position
+        u8g2Fonts.print(buff);
 
-            /* calculate the size of the box into which the text will fit */
-            lastH = u8g2Fonts.getFontAscent() - u8g2Fonts.getFontDescent();
-            lastW = u8g2Fonts.getUTF8Width(buff);
+        /* calculate the size of the box into which the text will fit */
+        lastH = u8g2Fonts.getFontAscent() - u8g2Fonts.getFontDescent();
+        lastW = u8g2Fonts.getUTF8Width(buff);
 
-            u8g2Fonts.setFont(u8g2_font_wqy16_t_gb2312a); // select u8g2 font from here: https://github.com/olikraus/u8g2/wiki/fntlistall
+        u8g2Fonts.setFont(u8g2_font_wqy16_t_gb2312a); // select u8g2 font from here: https://github.com/olikraus/u8g2/wiki/fntlistall
 
-            snprintf(buff, sizeof(buff), "星期%s", weekChars[lastWeek]);
+        snprintf(buff, sizeof(buff), "星期%s", weekChars[lastWeek]);
 
-            tbh = u8g2Fonts.getFontAscent() - u8g2Fonts.getFontDescent();
-            tbw = u8g2Fonts.getUTF8Width(buff);
+        tbh = u8g2Fonts.getFontAscent() - u8g2Fonts.getFontDescent();
+        tbw = u8g2Fonts.getUTF8Width(buff);
 
-            int16_t x, y;
-            x = ((display.width() - tbw) / 2) ;
-            y = ((display.height() - tbh) / 2) + 40  ;
+        int16_t x, y;
+        x = ((display.width() - tbw) / 2) ;
+        y = ((display.height() - tbh) / 2) + 40  ;
 
-            u8g2Fonts.setCursor(x, y);
-            u8g2Fonts.print(buff);
-            u8g2Fonts.setCursor(20, y + 50);
+        u8g2Fonts.setCursor(x, y);
+        u8g2Fonts.print(buff);
+        u8g2Fonts.setCursor(20, y + 50);
 
-            u8g2Fonts.print("你今天已经走了:");
-            u8g2Fonts.print("1000步");
+        u8g2Fonts.print("你今天已经走了:");
+        u8g2Fonts.print("1000步");
 
-            getX = u8g2Fonts.getCursorX();
-            getY = u8g2Fonts.getCursorY();
-            getH  = u8g2Fonts.getFontAscent() - u8g2Fonts.getFontDescent();
-            getW = u8g2Fonts.getUTF8Width("1000步");
+        getX = u8g2Fonts.getCursorX();
+        getY = u8g2Fonts.getCursorY();
+        getH  = u8g2Fonts.getFontAscent() - u8g2Fonts.getFontDescent();
+        getW = u8g2Fonts.getUTF8Width("1000步");
+        display.update();
 
-        } while (display.nextPage());
 
     } else {
         u8g2Fonts.setFont(u8g2_font_inr38_mn); // select u8g2 font from here: https://github.com/olikraus/u8g2/wiki/fntlistall
-        display.setPartialWindow(lastX, lastY - u8g2Fonts.getFontAscent() - 3, lastW, lastH);
-        display.firstPage();
-        do {
-
-            if (reverse) {
-                display.fillScreen(GxEPD_BLACK);
-                display.setTextColor(GxEPD_WHITE);
-
-            } else {
-                display.fillScreen(GxEPD_WHITE);
-                display.setTextColor(GxEPD_BLACK);
-            }
-
-
-            lastW = u8g2Fonts.getUTF8Width(buff);
-
-            u8g2Fonts.setCursor(lastX, lastY);
-            u8g2Fonts.print(buff);
-        } while (display.nextPage());
+        display.fillRect(lastX, lastY - u8g2Fonts.getFontAscent() - 3, lastW, lastH, GxEPD_WHITE);
+        display.fillScreen(GxEPD_WHITE);
+        display.setTextColor(GxEPD_BLACK);
+        lastW = u8g2Fonts.getUTF8Width(buff);
+        u8g2Fonts.setCursor(lastX, lastY);
+        u8g2Fonts.print(buff);
+        display.updateWindow(lastX, lastY - u8g2Fonts.getFontAscent() - 3, lastW, lastH, false);
     }
 }
 
@@ -234,8 +208,6 @@ void setup()
 
     btn->setPressedHandler([]() {
 
-        display.powerOff();
-
         delay(2000);
 
         esp_sleep_enable_ext1_wakeup(GPIO_SEL_36, ESP_EXT1_WAKEUP_ALL_LOW);
@@ -251,6 +223,7 @@ void setup()
 
     // Initialize the interface
     mainPage(true);
+
 
     // Reduce CPU frequency
     setCpuFrequencyMhz(40);
