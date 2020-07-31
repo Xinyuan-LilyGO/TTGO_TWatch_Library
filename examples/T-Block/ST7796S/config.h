39d0603f3a386098db404263001effd4c378f022
lewis he <lewisxhe@outlook.com>
2020-07-31 10:05:09 +0800

    Added tblock st7796 example

---
+++ examples/T-Block/ST7796S/config.h
@@ -0,0 +1,18 @@
+// => Hardware select
+#define LILYGO_WATCH_BLOCK               // To use T-Watch Block , please uncomment this line
+
+//NOT SUPPORT ...
+// //#define LILYGO_WATCH_2020_V1
+// #define LILYGO_WATCH_2019_WITH_TOUCH
+// #define LILYGO_WATCH_2019_NO_TOUCH
+//NOT SUPPORT ...
+
+// => Function select
+#define LILYGO_BLOCK_TOUCHSCREEN
+#define LILYGO_WATCH_LVGL                   //To use LVGL, you need to enable the macro LVGL
+
+#include <LilyGoWatch.h>
+
+
+
+
