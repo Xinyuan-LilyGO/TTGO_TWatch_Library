c03cc08a35e6bed74dce88b9fa883c4a6ff964da
NorthernDIY <70189433+NorthernDIY@users.noreply.github.com>
2020-09-03 00:41:35 -0500

    Update FT5206.h
    POWER_REG was defined as 0x87, this is the TIMENTERMONITOR register. 
    Suggestion: Replace incorrect POWER_REG define (0x87) with correct address 0xA5.
    Note: Writing 0x01 to 0xA5 puts the chip into monitor mode directly.
    Sidenote: TIMEENTERMONITOR register appears to have no function as all writes seem to produce no change to the default 30 second no touch timeout from active to monitor mode.

--- src/drive/fx50xx/FT5206.h
+++ src/drive/fx50xx/FT5206.h
@@ -38,7 +38,8 @@ github:https://github.com/lewisxhe/FT5206_Library
 #define FT5206_VENDID_REG       (0xA8)
 #define FT5206_CHIPID_REG       (0xA3)
 #define FT5206_THRESHHOLD_REG   (0x80)
-#define FT5206_POWER_REG        (0x87)
+#define FT5206_TIMEENTERMONITOR (0x87)  //Cant seem to change this?
+#define FT5206_POWER_REG        (0xA5)
 
 #define FT5206_MONITOR         (0x01)
 #define FT5206_SLEEP_IN        (0x03)
