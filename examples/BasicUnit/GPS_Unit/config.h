258204fd5999d77863d0fa15be5aa7d6a4606e06
lewis he <lewisxhe@outlook.com>
2020-09-21 14:43:26 +0800

    Added twatch-2020-V2 gps example

---
+++ examples/BasicUnit/GPS_Unit/config.h
@@ -0,0 +1,21 @@
+// => Hardware select
+#define LILYGO_WATCH_2020_V2
+
+
+// Only supports 2020 V2 version, other versions do not support
+
+//NOT SUPPORT ...
+// #define LILYGO_WATCH_2020_V1
+// #define LILYGO_WATCH_2019_WITH_TOUCH
+// #define LILYGO_WATCH_2019_NO_TOUCH
+// #define LILYGO_WATCH_BLOCK
+//NOT SUPPORT ...
+
+// => Function select
+#define LILYGO_WATCH_HAS_GPS
+
+#include <LilyGoWatch.h>
+
+
+
+
