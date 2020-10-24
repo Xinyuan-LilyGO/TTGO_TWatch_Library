b535ee32af42ad681d3d00b7777256e7d164b798
lewisxhe <lewisxhe@outlook.com>
2020-10-24 16:50:40 +0800

    Added TBao&TQucik examples

---
+++ examples/T_Quick/config.h
@@ -0,0 +1,23 @@
+// => Hardware select
+#define LILYGO_WATCH_2019_WITH_TOUCH     // To use T-Watch2019 with touchscreen, please uncomment this line
+// #define LILYGO_WATCH_2019_NO_TOUCH    // To use T-Watch2019 Not touchscreen , please uncomment this line
+
+
+//T- NOT SUPPORT ...
+// //#define LILYGO_WATCH_2020_V1
+// #define LILYGO_WATCH_BLOCK
+//NOT SUPPORT ...
+
+// => Function select
+#define LILYGO_WATCH_LVGL                   //To use LVGL, you need to enable the macro LVGL
+
+
+// Adapt to DRV8833 backplane
+// Adapt to DRV8833 backplane
+// Adapt to DRV8833 backplane
+
+#include <LilyGoWatch.h>
+
+
+
+
