9c5e29cce091973198b16f18e53fcadf60253dce
lewisxhe <lewishe@outlook.com>
2021-01-15 15:29:37 +0800

    Added V3 Microphone example

---
+++ examples/BasicUnit/TwatcV3Special/Microphone/config.h
@@ -0,0 +1,21 @@
+// => Hardware select
+#define LILYGO_WATCH_2020_V3             //To use T-Watch2020 V3, please uncomment this line
+
+
+//NOT SUPPORT ...
+//#define LILYGO_WATCH_2020_V1
+// #define LILYGO_WATCH_2020_V2
+// #define LILYGO_WATCH_2019_WITH_TOUCH
+// #define LILYGO_WATCH_2019_NO_TOUCH
+// #define LILYGO_WATCH_BLOCK
+//NOT SUPPORT ...
+
+// => Function select
+#define LILYGO_WATCH_LVGL                   //To use LVGL, you need to enable the macro LVGL
+
+
+#include <LilyGoWatch.h>
+
+
+
+
