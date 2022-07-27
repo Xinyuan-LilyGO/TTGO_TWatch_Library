ae7eeb2e3dc9c797bedc8f9a30e33332e2f4d1c6
lewisxhe <lewishe@outlook.com>
2022-07-27 11:24:20 +0800

    Update TTGO.h

--- src/TTGO.h
+++ src/TTGO.h
@@ -111,6 +111,11 @@ typedef FocalTech_Class CapacitiveTouch ;
 #include "libraries/GxEPD/src/GxIO/GxIO.h"
 #include "libraries/GxEPD/src/GxIO/GxIO_SPI/GxIO_SPI.h"
 #include "libraries/GxEPD/src/GxGDEP015OC1/GxGDEP015OC1.h"
+#elif defined(LILYGO_EINK_DEPG0150BN)
+#include "libraries/GxEPD/src/GxEPD.h"
+#include "libraries/GxEPD/src/GxIO/GxIO.h"
+#include "libraries/GxEPD/src/GxIO/GxIO_SPI/GxIO_SPI.h"
+#include "libraries/GxEPD/src/GxDEPG0150BN/GxDEPG0150BN.h"
 #elif  defined(LILYGO_EINK_GDEH0154D67_TP) || defined(LILYGO_EINK_GDEH0154D67_BL)
 #include "libraries/GxEPD/src/GxEPD.h"
 #include "libraries/GxEPD/src/GxIO/GxIO.h"
@@ -885,7 +890,7 @@ public:
     ePaperDisplay *ePaper = nullptr;
 #endif
 
-#if defined(LILYGO_EINK_GDEH0154D67_TP) || defined(LILYGO_EINK_GDEH0154D67_BL) || defined(LILYGO_EINK_GDEP015OC1)
+#if defined(LILYGO_EINK_GDEH0154D67_TP) || defined(LILYGO_EINK_GDEH0154D67_BL) || defined(LILYGO_EINK_GDEP015OC1) || defined(LILYGO_EINK_DEPG0150BN)
     GxIO_Class  *io = nullptr;
     GxEPD_Class *ePaper = nullptr;
 #endif
@@ -1306,7 +1311,7 @@ private:
         /* sclk = 18 mosi = 23 miso = n/a */
         SPI.begin(EINK_SPI_CLK, EINK_SPI_MISO, EINK_SPI_MOSI);
         ePaper = new ePaperDisplay( GDEW0371W7, EINK_BUSY, EINK_RESET, EINK_DC, EINK_SS );
-#elif defined(LILYGO_EINK_GDEH0154D67_BL) || defined(LILYGO_EINK_GDEH0154D67_TP) || defined(LILYGO_EINK_GDEP015OC1)
+#elif defined(LILYGO_EINK_GDEH0154D67_BL) || defined(LILYGO_EINK_GDEH0154D67_TP) || defined(LILYGO_EINK_GDEP015OC1) || defined(LILYGO_EINK_DEPG0150BN)
         log_i("GDEH0154D67 Init...");
 
         /* sclk = 18 mosi = 23 miso = n/a */
