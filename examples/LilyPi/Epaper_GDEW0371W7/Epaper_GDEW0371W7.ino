2729fac3b6a4f41767d6a8d0707054c32315c269
lewis <lewishe@outlook.com>
2023-04-28 20:25:04 +0800

    Clean project

--- examples/LilyPi/Epaper_GDEW0371W7/Epaper_GDEW0371W7.ino
+++
@@ -1,8 +0,0 @@
-/*
-Please jump to examples/T-Block/Epaper_GDEW0371W7 for examples,
-and comment #define LILYGO_WATCH_BLOCK before use
-And cancel the // #define LILYGO_LILYPI_V1 comment to use it normally
-*/
-
-
-
--- src/font/font.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FONT_H
-#define __FONT_H
-
-#include "../lvgl/lvgl.h"
-
-
-
-
-#endif  /*__FONT_H*/
