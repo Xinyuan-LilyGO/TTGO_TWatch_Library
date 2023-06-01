3f2a7a5e8b1e1f36d531bc985c3e8ab08af852cd
lewishe <lewishe@outlook.com>
2023-06-01 17:30:24 +0800

    Update epaper example

--- examples/T-Block/Epaper_TP/config.h
+++ examples/T-Block/Epaper_TP/config.h
@@ -1,6 +1,6 @@
 // => Hardware select
-#define LILYGO_WATCH_BLOCK               // To use T-Watch Block , please uncomment this line
-// #define LILYGO_WATCH_BLOCK_V1             // To use T-Watch Block V1 , please uncomment this line
+// #define LILYGO_WATCH_BLOCK               // To use T-Watch Block , please uncomment this line
+#define LILYGO_WATCH_BLOCK_V1             // To use T-Watch Block V1 , please uncomment this line
 
 //NOT SUPPORT ...
 // #define LILYGO_WATCH_2020_V1
@@ -10,7 +10,6 @@
 
 // => Function select
 #define LILYGO_EINK_GDEH0154D67_TP          //It is electronic paper with backlight
-
 #include <LilyGoWatch.h>
 
 
